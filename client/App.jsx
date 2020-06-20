1e4b67e62b699c7878ef3758d4caa38ae35a6a21
Kana-E <kana.eguchi@yahoo.com>
2020-06-19 23:17:12 -0400

    adds some basic html tags to render to client

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  render(){
-    return(
-      <div>This is a React Component</div>
-    )
+  componentDidMount() {
+    axios.get('/api/similaritems')
+      .then(data => {
+
+      });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div>Current Product</div>
+        <img src={this.photo}></img>
+        <button>Add to Cart</button>
+        <p></p>
+        <div>Similar Item 1</div>
+        <button>Add to Cart</button>
+        <div>Similar Item 2</div>
+        <button>Add to Cart</button>
+        <div>Similar Item 3</div>
+        <button>Add to Cart</button>
+        <div>Similar Item 4</div>
+        <button>Add to Cart</button>
+        <div>Similar Item 5</div>
+        <button>Add to Cart</button>
+      </div>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const port = 3000;
 const Guitar = require('../database/Model.js');
 
-app.use(express.static('./dist'));
+app.use (express.static('./dist'));
 
-app.get('/api/similaritems', (req, res) => {
+app.get ('/api/similaritems', (req, res) => {
   Guitar.find( (err, data) => {
     if (err) {
       console.log ('error finding similar items data');
@@ -14,6 +14,7 @@ app.get('/api/similaritems', (req, res) => {
       res.json(data);
     }
   });
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
