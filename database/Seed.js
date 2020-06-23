5eeee2187cce2db4f6885f4678d33af71f0e1ddf
Kana-E <kana.eguchi@yahoo.com>
2020-06-23 15:32:31 -0400

    formats the data in the db ($sign, Condition etc)

--- database/Seed.js
+++ database/Seed.js
@@ -2,36 +2,28 @@ const db = require('./index.js');
 var faker = require('faker');
 const Guitar = require('./Model.js');
 
-// const sampleImages = [
-//   {
-//     name: 'Cool Guitar',
-//     guitarImage: faker.image.imageUrl(),
-//     Ratings: '15',
-//     Price: faker.commerce.price(),
-//     Condition: 'New'
-//   },
-//   {
-//     name: 'Okay Guitar',
-//     guitarImage: faker.image.imageUrl(),
-//     Ratings: '5',
-//     Price: faker.commerce.price(),
-//     Condition: 'Old'
-//   }
-// ];
-
-
-
 const seedDB = function() {
   var data = [];
   var dataCount = 10;
 
   //create data set with random input for amount (dataCount)
+
+  var conditionMaker = function() {
+    var condition = Math.random();
+
+    if (condition === 0) {
+      return 'New';
+    } else {
+      return 'Old';
+    }
+  };
+
   for (var i = 0; i <= dataCount; i++) {
     var fakename = faker.commerce.productName();
     var fakeguitarImage = faker.image.imageUrl();
     var fakeRatings = Math.floor(Math.random() * 5);
-    var fakePrice = faker.commerce.price();
-    var fakeCondition = 'new';
+    var fakePrice = '$' + faker.commerce.price();
+    var fakeCondition = conditionMaker();
 
     var dataBulk = {
       name: fakename,
